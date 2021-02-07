7f4ce66baa5d49d35098332e916e9eb239df1126
Brad Broge <brbroge@gmail.com>
2021-02-07 16:08:26 -0500

    Adv-React basic setup of pages, and nav components

---
+++ sick-fits/frontend/components/Header.js
@@ -0,0 +1,12 @@
+import Nav from "./Nav";
+
+export default function Header() {
+  return (
+    <header>
+      <Nav />
+      <div className="sub-bar">
+        <p>SearchBar</p>
+      </div>
+    </header>
+  );
+}
